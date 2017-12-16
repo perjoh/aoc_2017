0672f9bd6332d0972f1625ff35c7ab90e2716747
perjoh <perjoh@gmail.com>
2017-12-16 16:20:26 +0100

    Puzzles 15 and 16

--- src/aoc_15.cpp
+++ src/aoc_15.cpp
@@ -1,6 +1,125 @@
 #include <iostream>
+#include <cassert>
+#include <regex>
+#include <fstream>
+#include <sstream>
+
+long long produce(long long prev_value, long long factor)
+{
+	return prev_value*factor%2147483647;
+}
+
+int count_part1(long long value_a, 
+				long long factor_a, 
+				long long value_b, 
+				long long factor_b)
+{
+	int count = 0;
+
+	for (int i = 0; i < 40000000; ++i) {
+		value_a = produce(value_a, factor_a); 
+		value_b = produce(value_b, factor_b); 
+
+		if ((value_a&0xffff) == (value_b&0xffff)) {
+			++count;
+		} 
+	}
+
+	return count;
+}
+
+long long produce_a(long long value, long long factor)
+{
+	do {
+		value = produce(value, factor);
+	} while ((value&3) != 0);
+	
+	return value;
+}
+
+long long produce_b(long long value, long long factor)
+{
+	do {
+		value = produce(value, factor);
+	} while ((value&7) != 0);
+
+	return value;
+}
+
+int count_part2(long long value_a,
+				long long factor_a,
+				long long value_b,
+				long long factor_b)
+{
+	int num_matches = 0;
+	int counter = 0;
+
+	const int num_iterations = 5000000;
+	while (counter < num_iterations) {
+
+		value_a = produce_a(value_a, factor_a);
+		value_b = produce_b(value_b, factor_b);
+
+		if ((value_a&0xffff) == (value_b&0xffff)) {
+			++num_matches;
+		}
+
+		++counter;
+	}
+
+	return num_matches;
+}
+
+int to_int(const std::string& value)
+{
+	std::stringstream ss(value);
+	int result = 0;
+	ss >> result;
+	return result;
+}
 
 int main()
 {
+	assert(count_part1(65, 16807, 8921, 48271) == 588);
+	assert(count_part2(65, 16807, 8921, 48271) == 309);
+
+	{
+		long long value_a = 65;
+		long long value_b = 8921;
+
+		long long expected_a[5] = { 1352636452, 1992081072, 530830436, 1980017072, 740335192 };
+		long long expected_b[5] = { 1233683848, 862516352, 1159784568, 1616057672, 412269392 };
+
+		for (int i = 0; i < 5; ++i) {
+			value_a = produce_a(value_a, 16807);
+			value_b = produce_b(value_b, 48271);
+			assert(value_a == expected_a[i]);
+			assert(value_b == expected_b[i]);
+		}
+	}
+
+	std::ifstream file("input/aoc_15.txt");
+	if (file) {
+		
+		long long value_a = 0;
+		long long value_b = 0;
+
+		std::string input;
+		while (std::getline(file, input)) {
+			static std::regex input_regex("Generator (A|B) starts with (\\d+)");
+			std::smatch match;
+			if (std::regex_search(input, match, input_regex)) {
+				assert(match.size() == 3);
+				if (match[1] == 'A') {
+					value_a = to_int(match[2]);
+				} else if (match[1] == 'B') {
+					value_b = to_int(match[2]);
+				}
+			}
+		} 
+		std::cout << "Part 1: " << count_part1(value_a, 16807, value_b, 48271) << "\n";
+		std::cout << "Part 2: " << count_part2(value_a, 16807, value_b, 48271) << "\n";
+	} 
+
 	return 0;
 }
--- src/aoc_16.cpp
+++ src/aoc_16.cpp
@@ -1,6 +1,120 @@
 #include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <sstream>
+#include <regex>
+#include <cassert>
+
+using string_vec = std::vector<std::string>;
+
+template <typename Stream>
+auto split_input(Stream& stream)
+{
+	string_vec result;
+	std::string input;
+	while (std::getline(stream, input, ',') && !input.empty()) {
+		result.push_back(input);
+	}
+	return result;
+}
+
+auto split_input(const char* input)
+{
+	std::stringstream ss(input);
+	return split_input(ss);
+}
+
+std::string spin(std::string programs, int value)
+{
+	std::string result(programs.cend() - value, programs.cend());
+	result.insert(result.end(), programs.cbegin(), programs.cbegin() + programs.length() - value);
+	return result;
+}
+
+std::string exchange(std::string programs, int a, int b)
+{
+	std::swap(programs[a], programs[b]);
+	return programs;
+}
+
+std::string partner(std::string programs, char a, char b)
+{
+	auto it_a = std::find(programs.begin(), programs.end(), a);
+	auto it_b = std::find(programs.begin(), programs.end(), b);
+
+	assert(it_a != programs.end());
+	assert(it_b != programs.end());
+
+	std::swap(*it_a, *it_b);
+
+	return programs;
+}
+
+int to_int(const std::string& s)
+{
+	std::stringstream ss(s);
+	int value = 0;
+	ss >> value;
+	return value;
+}
+
+std::string dance(std::string programs, const string_vec& dance_moves)
+{
+	for (auto& dance_move : dance_moves) {
+		static const std::regex s_regex("s(\\d+)");
+		static const std::regex x_regex("x(\\d+)/(\\d+)");
+		static const std::regex p_regex("p([a-p])/([a-p])");
+
+		std::smatch match;
+
+		if (std::regex_match(dance_move, match, s_regex)) {
+			programs = spin(programs, to_int(match[1]));
+		} else if (std::regex_match(dance_move, match, x_regex)) {
+			programs = exchange(programs, to_int(match[1]), to_int(match[2]));
+		} else if (std::regex_match(dance_move, match, p_regex)) {
+			programs = partner(programs, match[1].str()[0], match[2].str()[0]);
+        } else {
+            assert(false);
+        }
+	}
+
+	return programs;
+}
+
+void test()
+{
+	std::string programs("abcde");
+	programs = dance(programs, split_input("s1,x3/4,pe/b"));
+	assert(programs == "baedc");
+}
 
 int main()
 {
+	test();
+
+	const char* filename = "input/aoc_16.txt";
+	std::ifstream file(filename);
+	if (file) {
+		std::string programs("abcdefghijklmnop");
+		const std::string org_order = programs;
+		programs = dance(programs, split_input(file));
+		std::cout << "Part 1: " << programs << "\n";
+
+		programs = org_order;
+
+		// Testing reveals that the pattern wraps every 48 iteration.
+		for (int i = 0; i < 1000000000%48; ++i) {
+			file.close();
+			file.open(filename); 
+			programs = dance(programs, split_input(file));
+		}
+
+		std::cout << "Part 2: " << programs << "\n";
+
+	} else {
+		std::cout << "No input!\n";
+	}
+
 	return 0;
 }
