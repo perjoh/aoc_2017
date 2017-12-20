59a3a24488daf4ab4da53d6a29277557093ce901
perjoh <perjoh@gmail.com>
2017-12-20 20:03:14 +0100

    Puzzles 7 and 18

--- src/aoc_07.cpp
+++ src/aoc_07.cpp
@@ -1,17 +1,25 @@
 #include <iostream>
+#include <fstream>
 #include <string>
 #include <vector>
 #include <map>
+#include <regex>
+#include <cassert>
+#include <sstream>
+
+using namespace std; 
 
 struct Program 
 {
 	int weight{0};
 
-	std::string parent;
-	std::vector<std::string> children;
+	string parent;
+	vector<string> children;
 }; 
 
-void update_parents(std::map<std::string, Program>& programs)
+using Program_map = map<string, Program>;
+
+void update_parents(Program_map& programs)
 {
 	for (auto& pair : programs) {
 		for (auto& child : pair.second.children) {
@@ -20,21 +28,120 @@ void update_parents(std::map<std::string, Program>& programs)
 	}
 }
 
-std::string find_root(const std::map<std::string, Program>& programs)
+std::string find_root(const Program_map& programs)
 {
 	for (auto& pair : programs) {
-		if (pair->second.parent.empty()) {
-			return pair->first;
+		if (pair.second.parent.empty()) {
+			return pair.first;
 		}
 	}
 
 	return std::string();
 }
 
+int to_int(const string& s)
+{
+	int value = 0;
+	stringstream ss(s);
+	ss >> value;
+	return value; 
+}
+
+pair<string, Program> parse_input(const string& input)
+{
+	vector<string> names;
+	static regex regex_names("([a-z]+)");
+	smatch m;
+	string::const_iterator input_it = input.cbegin();
+	while (regex_search(input_it, input.cend(), m, regex_names)) {
+		names.push_back(m[1]);
+		input_it = m[0].second;
+	}
+
+	static regex regex_weight("\\(([0-9]+)\\)");
+	regex_search(input, m, regex_weight); 
+
+	Program program;
+	program.weight = to_int(m[1]);
+	program.children.insert(program.children.begin(), names.cbegin() + 1, names.cend()); 
+
+	return make_pair(names[0], program);
+}
+
+int sum_weight(const Program_map& programs, const string& prg_name)
+{
+	int sum = 0;
+	auto& program = programs.find(prg_name)->second;
+
+	for (auto& child : program.children) {
+		sum += sum_weight(programs, child);
+	}
+
+	return sum;
+}
+
+bool is_balanced(const Program_map& programs, const string& prg_name)
+{
+	auto& program = programs.find(prg_name)->second;
+	if (!program.children.empty()) {
+		int weight = sum_weight(programs, program.children[0]);
+		for (auto it = program.children.cbegin() + 1; it != program.children.cend(); ++it) {
+			if (weight != sum_weight(programs, *it)) {
+				return false;
+			}
+		}
+	}
+
+	return true;
+}
+
+string find_unbalanced(const Program_map& programs, const string& root_prg) 
+{
+	auto& program = programs.find(root_prg)->second;
+
+	for (auto& child_prg : program.children) {
+		if (!is_balanced(programs, child_prg)) {
+			return find_unbalanced(programs, child_prg);
+		}
+	} 
+
+	return root_prg;
+}
+
+void test()
+{
+	auto program = parse_input("fwft (72) -> ktlj, cntj, xhth");
+	assert(program.first == "fwft");
+	assert(program.second.weight == 72);
+	assert(program.second.children[0] == "ktlj");
+	assert(program.second.children[1] == "cntj");
+	assert(program.second.children[2] == "xhth");
+}
+
 int main()
 {
-	std::map<std::string, Program> programs;
+	test();
 
+	ifstream file("input/aoc_07.txt");
+	if (file) {
+
+		Program_map programs;
+
+		string input;
+		while (getline(file, input) && !input.empty()) {
+			auto program = parse_input(input);
+			programs[program.first] = program.second;
+		}
+
+		update_parents(programs);
+
+		assert(count_if(programs.cbegin(), programs.cend(), [](auto& prg){ return prg.second.parent.empty(); }) == 1);
+
+		const string& root = find_root(programs);
+		cout << "Part 1: " << root << "\n";
+
+		cout << "Part 2: " << find_unbalanced(programs, root) << "\n";
+	}
 
 	return 0;
 }
--- src/aoc_18.cpp
+++ src/aoc_18.cpp
@@ -1,6 +1,263 @@
 #include <iostream>
+#include <fstream>
+#include <vector>
+#include <deque>
+#include <map>
+#include <sstream>
+#include <functional>
+#include <cassert>
+
+using namespace std;
+
+long long to_value(const string& s)
+{
+	long long value;
+	stringstream ss(s);
+	ss >> value;
+	return value;
+}
+
+using Registers = map<string, long long>;
+
+struct Instruction 
+{
+	Instruction(const string& instr)
+	{
+		stringstream ss(instr);
+
+		ss >> op;
+		ss >> arg0;
+		ss >> arg1;
+	}
+
+	string op;
+	string arg0, arg1;
+};
+
+class Cpu
+{
+public :
+	Cpu()
+	{
+		using placeholders::_1;
+		using placeholders::_2;
+
+
+		instruction_set["snd"] = bind(&Cpu::snd, this, _1, _2);
+		instruction_set["set"] = bind(&Cpu::set, this, _1, _2);
+		instruction_set["add"] = bind(&Cpu::add, this, _1, _2);
+		instruction_set["mul"] = bind(&Cpu::mul, this, _1, _2);
+		instruction_set["mod"] = bind(&Cpu::mod, this, _1, _2);
+		instruction_set["rcv"] = bind(&Cpu::rcv, this, _1, _2);
+		instruction_set["jgz"] = bind(&Cpu::jgz, this, _1, _2); 
+	}
+
+	void connect(Cpu* other_, long long id)
+	{
+		other = other_;
+		registers["p"] = id;
+	}
+
+	int run_program(const vector<Instruction>& program)
+	{
+        auto check_pc = [&program](auto pc) -> bool { return 0 <= pc && static_cast<size_t>(pc) < program.size();  };
+        jmp = 1;
+		int num_exec = 0;
+		while (check_pc(pc) && !waiting()) {
+			const auto& p = program[pc];
+			jmp = 1;
+			instruction_set[p.op](p.arg0, p.arg1);
+			pc += jmp;
+			if (jmp != 0) {
+				++num_exec;
+			}
+		}
+
+		if (!check_pc(pc)) {
+			terminate();
+		}
+
+		return num_exec;
+	}
+
+	long long freq_recovered() const 
+	{
+		return freq_playing;
+	}
+
+	bool waiting() const 
+	{
+		return jmp == 0;
+	}
+
+	bool terminated() const
+	{
+		return pc < 0;
+	}
+
+	int num_sends() const
+	{
+		return num_sends_;
+	}
+
+private :
+	using Instruction_set = map<string, function<void(const string&, const string&)>>;
+	Instruction_set instruction_set;
+
+	int pc{0};
+	int jmp{1};
+	Registers registers;
+
+	long long freq_playing{0};
+	int num_sends_{0};
+
+	Cpu* other{nullptr};
+	deque<long long> queue;
+
+private :
+	void snd(const string& arg0, const string&)
+	{
+		if (other) { 
+			other->queue.push_back(value(arg0));
+			++num_sends_;
+		} else {
+			freq_playing = value(arg0);
+		} 
+	}
+
+	void set(const string& arg0, const string& arg1)
+	{
+		registers[arg0] = value(arg1);
+	}
+
+	void add(const string& arg0, const string& arg1)
+	{
+		registers[arg0] += value(arg1);
+	}
+
+	void mul(const string& arg0, const string& arg1)
+	{
+		registers[arg0] *= value(arg1);
+	}
+
+	void mod(const string& arg0, const string& arg1)
+	{
+		registers[arg0] %= value(arg1);
+	}
+
+	void rcv(const string& arg0, const string&)
+	{
+		if (other) {
+			if (!queue.empty()) {
+				registers[arg0] = queue.front();
+				queue.pop_front();
+			} else {
+				jmp = 0; // waiting
+			}
+		} else {
+
+			if (value(arg0) != 0) {
+				if (freq_playing != 0) {
+					terminate();
+				}
+			}
+		}
+	}
+
+	void jgz(const string& arg0, const string& arg1)
+	{ 
+		if (value(arg0) > 0) {
+			jmp = static_cast<int>(value(arg1));
+		}
+	}
+
+	void terminate()
+	{
+		pc = -2;
+	}
+
+private :
+	long long value(const string& arg) {
+		if ('a' <= arg[0] && arg[0] <= 'z') {
+			return registers[arg];
+		} 
+
+		return to_value(arg);
+	} 
+}; 
+
+template <typename Stream>
+vector<Instruction> read_program(Stream& stream)
+{
+	vector<Instruction> program;
+	string input;
+	while (getline(stream, input) && !input.empty()) {
+		Instruction instruction(input);
+		program.push_back(instruction);
+	}
+	return program;
+}
+
+void test()
+{
+	const char* source = 	"set a 1\n"
+							"add a 2\n"
+							"mul a a\n"
+							"mod a 5\n"
+							"snd a\n"
+							"set a 0\n"
+							"rcv a\n"
+							"jgz a -1\n"
+							"set a 1\n"
+							"jgz a -2\n";
+	stringstream ss(source); 
+	auto program = read_program(ss);
+	
+	Cpu cpu;
+	cpu.run_program(program);
+	assert(cpu.freq_recovered() == 4);
+}
 
 int main()
 {
+	test();
+
+	ifstream file("input/aoc_18.txt");
+	if (file) { 
+		auto program = read_program(file);
+
+		Cpu cpu;
+		cpu.run_program(program);
+
+		cout << "Part 1: " << cpu.freq_recovered() << "\n";
+
+		Cpu cpu0; 
+		Cpu cpu1; 
+
+		cpu0.connect(&cpu1, 0);
+		cpu1.connect(&cpu0, 1);
+
+		bool is_deadlock = false;
+		while (!is_deadlock) {
+			cpu0.run_program(program);
+			cpu1.run_program(program);
+
+            if (cpu0.waiting() && cpu1.waiting()) {
+                const int num_inst0 = cpu0.run_program(program);
+                const int num_inst1 = cpu1.run_program(program); 
+
+                if (num_inst0 == 0 && num_inst1 == 0) {
+                    is_deadlock = true;
+                }
+            }
+
+			if ((cpu0.waiting() && cpu1.terminated()) || (cpu1.waiting() && cpu0.terminated())) {
+				is_deadlock = true;
+			}
+		}
+
+		cout << "Part 2: " << cpu1.num_sends() << "\n";
+	}
+
 	return 0;
 }
